2c9978b0b6e137be8e2bea641458a2ecbadf3304
Oleksandr Radchenko <oradchenko108@gmail.com>
2024-11-05 15:38:48 +0530

    created user schemas

---
+++ src/schemas/user.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import Annotated
+
+from annotated_types import MinLen, MaxLen
+from pydantic import BaseModel, EmailStr
+
+
+class UserBaseSchema(BaseModel):
+    username: Annotated[str, MinLen(3), MaxLen(20)]
+    email: EmailStr
+
+
+class UserCreateSchema(UserBaseSchema):
+    password: str
+
+
+class UserUpdateSchema(UserCreateSchema):
+    pass
+
+
+class UserUpdatePartialSchema(UserCreateSchema):
+    username: Annotated[str, MinLen(3), MaxLen(20)] | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+
+
+class UsersSchema(UserBaseSchema):
+    id: int
+
+
+class UserSchema(UserBaseSchema):
+    id: int
+    is_active: bool = True
+    created_at: datetime
